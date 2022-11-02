f5a4edf3549e97ea4f5b40e4abc9729ceeb2479c
Julian Osorio <juliandresog@yahoo.com>
2022-11-02 17:05:50 -0500

    Pruebas con richtext

--- app/view/CascadingSelectViewController.js
+++ app/view/CascadingSelectViewController.js
@@ -217,7 +217,8 @@ Ext.define('CascadingSelect.view.CascadingSelectViewController', {
 
 
         //var editor1 = new RichTextEditor("#div_editor1", { editorResizeMode: "height" });
-        var editor1 = new RichTextEditor("#div_editor_boos", config);
+        //var editor1 = new RichTextEditor("#div_editor_boos", config);
+        var editor1 = new RichTextEditor("#div_editor_boos");
         //editor1.setHTMLCode("Use inline HTML or setHTMLCode to init the default content.");
     }
 
